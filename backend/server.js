9fa48937ee445836e7239fa56bfeb91d1093ae50
FannyM12 <fannymorales214@gmail.com>
2026-02-23 21:53:43 -0600

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const SECRET_KEY = "clave_super_secreta";
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  const token = jwt.sign(
+    { email: email },
+    SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+
+  res.json({ token });
+});
+
+app.listen(5000, () => {
+  console.log("Servidor corriendo en http://localhost:5000");
+});
