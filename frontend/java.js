89aa5405f87bf78b51eaee2499d0f979f1a02a8b
FannyM12 <fannymorales214@gmail.com>
2026-02-23 20:39:15 -0600

    Update java.js

--- frontend/java.js
+++ frontend/java.js
@@ -1,1 +1,41 @@
+ const form = document.getElementById("login");
 
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("pass").value;
+        const message = document.getElementById("message");
+
+        // validar email
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailRegex.test(email)) {
+            message.textContent = "Correo no valido";
+            return;
+        }
+        //validar password
+        const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).+$/;
+
+        if (!passwordRegex.test(password)) {
+            message.textContent = "La contraseña debe tener mayuscula, numero y simbolo";  
+            return;
+        }
+
+        try {
+            const response = await fetch("http://localhost:/base", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ email, pass })
+            });
+
+            const data = await response.json();
+
+            message.textContent = "el token funciona: " + data.token;
+
+        } catch (error) {
+            message.textContent = "Error al conectar con el servidor";
+        }
+    });
